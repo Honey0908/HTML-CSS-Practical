c99480713e192886c02dc8ab169bbb68952e2e04
Honey Patel <honey.p@simformsolutions.com>
2026-02-27 16:03:01 +0530

    Refactor index.js for improved readability and consistency in formatting

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-const left_control=document.getElementById("left-control");
-const right_control=document.getElementById("right-control");
+const left_control = document.getElementById('left-control');
+const right_control = document.getElementById('right-control');
 
 // Product List1
-const list=document.getElementsByClassName("product-list-items")[0];
+const list = document.getElementsByClassName('product-list-items')[0];
 // width of each Item
-const itemWidth=list.children[0].offsetWidth;
+const itemWidth = list.children[0].offsetWidth;
 
 // show and hide left and right controlls
-function show_hide_buttons(){
-    let scrollWidth= list.scrollWidth - list.clientWidth;
-    left_control.style.display= list.scrollLeft=== 0 ? "none" : "block";
-    right_control.style.display= list.scrollLeft === scrollWidth ?"none":"block";
+function show_hide_buttons() {
+  let scrollWidth = list.scrollWidth - list.clientWidth;
+  left_control.style.display = list.scrollLeft === 0 ? 'none' : 'block';
+  right_control.style.display =
+    list.scrollLeft === scrollWidth ? 'none' : 'block';
 }
 
 show_hide_buttons();
 
+console.log(itemWidth);
+
+const a;
+if (a > 0) {
+  console.log('a is greater than 0');
+}
 
 // 3 items will be scrolled on right
-left_control.addEventListener("click", ()=>{
-    list.scrollTo(list.scrollLeft-itemWidth*3,0)
-    show_hide_buttons();
+left_control.addEventListener('click', () => {
+  list.scrollTo(list.scrollLeft - itemWidth * 3, 0);
+  show_hide_buttons();
 });
 
 // 3 items will be scrolled on left
-right_control.addEventListener("click", ()=>{
-    list.scrollTo(list.scrollLeft+itemWidth*3,0)
-    show_hide_buttons();
+right_control.addEventListener('click', () => {
+  list.scrollTo(list.scrollLeft + itemWidth * 3, 0);
+  show_hide_buttons();
 });
-
